e118dd165a18c29a77b1231fa8496e4ca582a4e9
NT-Huyen <2051060557@e.tlu.edu.vn>
2022-03-19 10:27:57 +0700

    yêu cầu 1

---
+++ Yêu cầu 1/main.js
@@ -0,0 +1,45 @@
+// add 'Meat' in the beginning of your shopping cart if it has not been already added
+//     const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
+//     document.getElementById('demo').innerHTML = shoppingCart
+    
+//     if(shoppingCart.includes('Meat')){
+//         //console.log('exited')
+//         document.getElementById('demo').innerHTML ='existed'
+
+//     }
+//     else{
+//         shoppingCart.unshift('Meat')
+//         //console.log(shoppingCart)
+//         document.getElementById('demo').innerHTML =shoppingCart
+//     }
+
+//    - add Sugar at the end of you shopping cart if it has not been already added
+//     const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
+//     document.getElementById('demo').innerHTML = shoppingCart
+//     if(shoppingCart.includes('Sugar')){
+//         document.getElementById('demo').innerHTML ='existed'
+//     }
+//     else{
+//         shoppingCart.push('Sugar')
+//         document.getElementById('demo').innerHTML = shoppingCart
+//     }
+
+//    - remove 'Honey' if you are allergic to honey
+
+//    - modify Tea to 'Green Tea'
+//     const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
+//     document.getElementById('demo').innerHTML = shoppingCart
+//     for(let i=0; i<shoppingCart.length;i++){
+//         if(shoppingCart[i] == 'Tea'){
+//             shoppingCart[i] = 'Green Tea'
+//         }
+//     }
+//     document.getElementById('demo').innerHTML = shoppingCart
+     
+// Write a function which generates a _randomUserIp_.
+// function generateRandomUserIp() {
+//     return (Math.floor(Math.random() * 256 ))+'.'+Math.floor(Math.random() * 256 )+'.'+(Math.floor(Math.random() * 256))+'.'+(Math.floor(Math.random() * 256))
+// }
+// console.log("Generating random IP "+ generateRandomUserIp())
+
+// Write a function which generates a _randomMacAddress_
